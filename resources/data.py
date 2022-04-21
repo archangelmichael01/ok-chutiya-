f67a2bcbbe9f3b90031145cabb21caad1955ff8f
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-04-22 00:36:16 +0530

    Update data.py

--- resources/data.py
+++ resources/data.py
@@ -1,10 +1,11 @@
-GRP = ["@DNHxHELL|@RiZoeLXSpam|@ArrayCoreChats|DNHxHELL|RiZoeLXSpam|ArrayCoreChats"]
+GRP = [
+"@DNHxHELL|@RiZoeLXSpam|@ArrayCoreChats|DNHxHELL|RiZoeLXSpam|ArrayCoreChats"
 
 GROUP = [-1001321613309, -1001771478102, -1001749467927]
 
 RiZoeLX = [1517994352, 2086101519, 1854700253]
 
-TheRiZoeL = ["@TheRiZoeL|@TheVenomxd|RiZoeL"]
+TheRiZoeL = "@TheRiZoeL|@TheVenomxd|RiZoeL"
 
 REPLYRAID = [
     "𝙈𝘼𝘿𝘼𝙍𝘾𝙃𝙊𝘿",
