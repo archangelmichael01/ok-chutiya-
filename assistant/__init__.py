ecafe1e1181f3014ebd41346b2051fa3382ce849
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-04-22 11:21:22 +0530

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -3,6 +3,6 @@ import sys
 import asyncio
 import telethon.utils
 from telethon import TelegramClient
-from RiZoeLXSpam import BOT_TOKEN
+from RiZoeLXSpam import BOT_TOKEN, API_ID, API_HASH
 
 RiZoeL = TelegramClient('RiZoeL', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
