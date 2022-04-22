8de135bf7ebeeadab4f1bfaa9fd809b0170754c5
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-04-22 13:13:23 +0530

    Update spam.py

--- assistant/plugins/spam.py
+++ assistant/plugins/spam.py
@@ -8,7 +8,6 @@ from resources.data import *
 from telethon.tl import functions
 from RiZoeLXSpam import OWNER_ID, hl, CLIENTS
 from assistant import RiZoeL
-from RiZoeLXSpam import (id_1, id_2, id_3, id_4, id_5 , id_6, id_7, id_8, id_9, id_10, id_11, id_12, id_13, id_14, id_15, id_16, id_17, id_18, id_19, id_20, id_21, id_22, id_23, id_24, id_25, id_26, id_27, id_28, id_29, id_30, id_31, id_32, id_33, id_34, id_35, id_36, id_37, id_38, id_39, id_40)
 
 SEND_GRP = "**Send Group Link Where You want to spam**"
 SEND_COUNT ="**Send Counts**"
