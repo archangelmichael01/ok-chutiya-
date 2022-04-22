8bafb78ea0a9053c35d2aeb38eea3b9b0f4595e3
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-04-22 22:39:40 +0530

    Update spam.py

--- assistant/plugins/spam.py
+++ assistant/plugins/spam.py
@@ -148,106 +148,106 @@ async def spam(event):
             #  while count != 0:
               if Riz:
                   await Riz.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz2:
                   await Riz2.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz3:
                   await Riz3.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz4:
                   await Riz4.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz5:
                   await Riz5.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz6:
                   await Riz6.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz7:
                   await Riz7.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz8:
                   await Riz8.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                $  await asyncio.sleep(0.3)
               if Riz9:
                   await Riz9.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz10:
                   await Riz10.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz11:
                   await Riz11.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz12:
                   await Riz12.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz13:
                   await Riz13.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz14:
                   await Riz14.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz15:
                   await Riz15.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz16:
                   await Riz16.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz17:
                   await Riz17.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz18:
                   await Riz18.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                # await asyncio.sleep(0.3)
               if Riz19:
                   await Riz19.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz20:
                   await Riz20.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz21:
                   await Riz21.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz22:
                   await Riz22.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz23:
                   await Riz23.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz24:
                   await Riz24.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz25:
                   await Riz25.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz26:
                   await Riz26.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz27:
                   await Riz27.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz28:
                   await Riz28.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz29:
                   await Riz29.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz30:
                   await Riz30.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz31:
                   await Riz31.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz32:
                   await Riz31.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz33:
                   await Riz33.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                  #await asyncio.sleep(0.3)
               if Riz34:
                   await Riz34.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
               if Riz35:
                   await Riz35.send_message(grp_id, message)
               if Riz36:
@@ -256,12 +256,12 @@ async def spam(event):
                   await Riz37.send_message(grp_id, message)
               if Riz38:
                   await Riz38.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 $ await asyncio.sleep(0.3)
               if Riz39:
                   await Riz39.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                #  await asyncio.sleep(0.3)
               if Riz40:
                   await Riz40.send_message(grp_id, message)
-                  await asyncio.sleep(0.3)
+                 # await asyncio.sleep(0.3)
         except Exception as ex:
                 print(ex)
