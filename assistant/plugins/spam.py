c6ce0b4c7a2327d0117f8b3b7be2f7348b66abe9
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-04-26 19:51:24 +0530

    Update spam.py

--- assistant/plugins/spam.py
+++ assistant/plugins/spam.py
@@ -15,12 +15,15 @@ SEND_SPAM = "**Send spam message to spam**"
 ABORT = "**All process Cancelled !**"
 
 
+client = [Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, Riz11, Riz12, Riz13, Riz14, Riz15, Riz16, Riz17, Riz18, Riz19, Riz20, Riz21, Riz22, Riz23, Riz24, Riz25, Riz26, Riz27, Riz28, Riz29, Riz30, Riz31, Riz32, Riz33, Riz34, Riz35, Riz36, Riz37, Riz38, Riz39, Riz40]
 
-
+async def send_msg(spam, chat, msg):
+    await spam.send_message(chat, msg)
 
 @RiZoeL.on(events.NewMessage(pattern="[!/]spam"))
 async def spam(event):
-  if event.sender_id in DEV or event.sender_id in SUDO_USERS:
+     if event.sender_id not in DEV or event.sender_id not in SUDO_USERS:
+             return
      if event.is_group:
           return await event.reply("**Use This Cmd In PM**")
      async with RiZoeL.conversation(event.chat_id) as rizx:
@@ -31,12 +34,12 @@ async def spam(event):
             return await rizx.send_message(ABORT)
         grpp = res.message.message
         if grpp.startswith("https://t.me/") or grpp.startswith("@"):
-              Group = grpp
-              if re.search(GRP.lower(), Group.lower()):
+             Group = grpp
+             if re.search(GROUPP.lower(), Group.lower()):
                    return await rizx.send_message("Sorry !! I can't Spam there")
-              k = await Riz.get_entity(Group)
-              grp_id = k.id
-              try:
+             k = await Riz.get_entity(Group)
+             grp_id = k.id
+             try:
                  if Riz:
                        await Riz(functions.channels.JoinChannelRequest(channel=Group))
                  if Riz2:
@@ -117,8 +120,10 @@ async def spam(event):
                        await Riz39(functions.channels.JoinChannelRequest(channel=Group))
                  if Riz40:
                        await Riz40(functions.channels.JoinChannelRequest(channel=Group))
-              except Exception as ex:
+             except Exception as ex:
                       print(ex)
+                      rizx.send_message(f"Error !! \n\n {ex}")
+                      return
         else:
             await rizx.send_message("Error! Send Group link or Username")
             return
@@ -139,129 +144,15 @@ async def spam(event):
         if "/cancel" in res.message.message:
             return await rizx.send_message(ABORT)
         message = res.message.message
+        if re.search(TheRiZoeL.lower(), message.lower()):
+                   return await rizx.send_message("Sorry !! I can't Spam On @RiZoeLX's Owner")
         Fukoff = await rizx.send_message(f"__Starting Spam In {Group}__")
-        await asyncio.sleep(2)
-        await Fukoff.edit(f"**▪️Started Spam ▪️** \n\n__Group__ : `{Group}`\n__Spam Count__ : `{count}` \n__Spam Message__ : `{message}`")     
-        ids = 0
         try:
            for _ in range(count):
-            #  while count != 0:
-              if Riz:
-                  await Riz.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz2:
-                  await Riz2.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz3:
-                  await Riz3.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz4:
-                  await Riz4.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz5:
-                  await Riz5.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz6:
-                  await Riz6.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz7:
-                  await Riz7.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz8:
-                  await Riz8.send_message(grp_id, message)
-                # await asyncio.sleep(0.3)
-              if Riz9:
-                  await Riz9.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz10:
-                  await Riz10.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz11:
-                  await Riz11.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz12:
-                  await Riz12.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz13:
-                  await Riz13.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz14:
-                  await Riz14.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz15:
-                  await Riz15.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz16:
-                  await Riz16.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz17:
-                  await Riz17.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz18:
-                  await Riz18.send_message(grp_id, message)
-                # await asyncio.sleep(0.3)
-              if Riz19:
-                  await Riz19.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz20:
-                  await Riz20.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz21:
-                  await Riz21.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz22:
-                  await Riz22.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz23:
-                  await Riz23.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz24:
-                  await Riz24.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz25:
-                  await Riz25.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz26:
-                  await Riz26.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz27:
-                  await Riz27.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz28:
-                  await Riz28.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz29:
-                  await Riz29.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz30:
-                  await Riz30.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz31:
-                  await Riz31.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz32:
-                  await Riz31.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz33:
-                  await Riz33.send_message(grp_id, message)
-                  #await asyncio.sleep(0.3)
-              if Riz34:
-                  await Riz34.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz35:
-                  await Riz35.send_message(grp_id, message)
-              if Riz36:
-                  await Riz36.send_message(grp_id, message)
-              if Riz37:
-                  await Riz37.send_message(grp_id, message)
-              if Riz38:
-                  await Riz38.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
-              if Riz39:
-                  await Riz39.send_message(grp_id, message)
-                #  await asyncio.sleep(0.3)
-              if Riz40:
-                  await Riz40.send_message(grp_id, message)
-                 # await asyncio.sleep(0.3)
+                for x in client:
+                     await send_msg(x, grp_id, message)
+                     await asyncio.sleep(0.3)
+           await Fukoff.edit(f"**▪️Started Spam ▪️** \n\n__Group__ : `{Group}`\n__Spam Count__ : `{count}` \n__Spam Message__ : `{message}`")
         except Exception as ex:
-                print(ex)
+               rizx.send_message(f"Error !! \n\n {ex}")
+               print(ex)
