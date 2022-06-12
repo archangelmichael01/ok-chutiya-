315e22da8f93fe09eb3f62fe1f89ef9fc784a139
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-06-12 16:14:33 +0530

    Update scrape.py

--- SpamX/module/scrape.py
+++ SpamX/module/scrape.py
@@ -6,31 +6,31 @@ from SpamX import (RiZoeL1, RiZoeL2, RiZoeL3, RiZoeL4, RiZoeL5,
                 RiZoeL6, RiZoeL7, RiZoeL8, RiZoeL9, RiZoeL10, 
                 RiZoeL11, RiZoeL12, RiZoeL13, RiZoeL14, RiZoeL15, 
                 RiZoeL16, RiZoeL17, RiZoeL18, RiZoeL19, RiZoeL20, HNDLR,
-                SUDO_USERS, hl)
+                OWNER_ID, hl)
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@RiZoeL.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL2.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL3.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL4.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL5.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL6.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL7.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL8.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL9.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL10.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL11.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL12.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL13.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL14.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL15.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL16.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL17.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL18.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL19.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
-@RiZoeL20.on_message(filters.user(SUDO_USERS) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL2.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL3.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL4.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL5.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL6.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL7.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL8.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL9.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL10.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL11.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL12.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL13.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL14.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL15.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL16.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL17.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL18.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL19.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
+@RiZoeL20.on_message(filters.user(OWNER_ID) & filters.command(["scrape"], prefixes=HNDLR))
 async def scrape(_, e: Message):
       chat_id = e.chat.id
       user_id = e.fron_user.id
@@ -54,4 +54,5 @@ async def scrape(_, e: Message):
                except Exception as ex:
                  await e.send_message(chat_id, f"**Error !!** \n\n {ex}")
                  print(ex)
+
            return await e.send_message(chat_id, f"**Scraped Users** \n\n **From Chat:** {scrape_grp} \n **Users Added:** {add}")
