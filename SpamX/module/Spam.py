e67c95bdbcab943d4b1940e7be66a69fda73e390
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-06-13 16:19:00 +0530

    Update Spam.py

--- SpamX/module/Spam.py
+++ SpamX/module/Spam.py
@@ -3,7 +3,7 @@ import sys
 import asyncio
 import re
 from random import choice
-from SpamX import (RiZoeL1, RiZoeL2, RiZoeL3, RiZoeL4, RiZoeL5, 
+from SpamX import (RiZoeL, RiZoeL2, RiZoeL3, RiZoeL4, RiZoeL5, 
                 RiZoeL6, RiZoeL7, RiZoeL8, RiZoeL9, RiZoeL10, 
                 RiZoeL11, RiZoeL12, RiZoeL13, RiZoeL14, RiZoeL15, 
                 RiZoeL16, RiZoeL17, RiZoeL18, RiZoeL19, RiZoeL20, HNDLR,
